781caa03fb975b15439e9b461725b001ef398b5f
Lisa Huang <lisa.yc.huang@gmail.com>
2018-09-30 20:51:03 -0700

    Add app logo and add welcome text

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notifications: []
+    };
+  }
+
+  handleOnClick = event => {
+    // // Handle Form Submit event default
+    // // Create quote object from state
+    // // Pass quote object to action creator
+    
+    event.preventDefault();
+    // fetch("https://api.example.com/items")
+    //   .then(res => res.json())
+    //   .then(
+    //     (result) => {
+    //       this.setState({
+    //         isLoaded: true,
+    //         items: result.items
+    //       });
+    //     },
+    //     // Note: it's important to handle errors here
+    //     // instead of a catch() block so that we don't swallow
+    //     // exceptions from actual bugs in components.
+    //     (error) => {
+    //       this.setState({
+    //         isLoaded: true,
+    //         error
+    //       });
+    //     }
+    //   )
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Gitmailz</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+
+          <button onClick={this.handleOnClick}>Login</button>
+          <a href="https://gitmailz-api.herokuapp.com/signin">Login here</a>
         </p>
       </div>
     );
