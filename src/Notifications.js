938689d0127ef5678ea953a64efe8bf0ae945555
Lisa Huang <lisa.yc.huang@gmail.com>
2018-10-03 15:02:10 -0700

    Install react-data-sort. Refactor notifications to use data table

--- src/Notifications.js
+++ src/Notifications.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { endpoint } from "./api";
 import { notification } from "./Seed";
+import DataTable from './Datatable'
 
 export default class Notifications extends React.Component {
   constructor(props) {
@@ -48,19 +49,25 @@ export default class Notifications extends React.Component {
       return (
         <div>
           <h2>My Notifications </h2>
-          <div className="notification-container">
-            {notifications.map(n => (
-              <div className="notification" key={n.id}>
-              Repo: <a href={n.repository.html_url}>{n.repository.name}</a><br></br>
-                {n.subject.type}: <a href={n.subject.url}>{n.subject.title}</a><br></br>
-                Reason: {n.reason}<br></br>
-                Update date: {n.updated_at.slice(0,10)}
-                <hr></hr>
-              </div>
-            ))}
-          </div>
+
+          <DataTable/>
         </div>
       );
     }
   }
 // }
+
+
+// ORIGINAL UI TO DISPLAY NOTIFICATIONS
+
+// <div className="notification-container">
+// {notifications.map(n => (
+//   <div className="notification" key={n.id}>
+//   Repo: <a href={n.repository.html_url}>{n.repository.name}</a><br></br>
+//     {n.subject.type}: <a href={n.subject.url}>{n.subject.title}</a><br></br>
+//     Reason: {n.reason}<br></br>
+//     Update date: {n.updated_at.slice(0,10)}
+//     <hr></hr>
+//   </div>
+// ))}
+// </div>
