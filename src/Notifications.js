be97d9005523ff42b9b524594ffdc784b7f190bb
Lisa Huang <lisa.yc.huang@gmail.com>
2018-10-03 12:08:35 -0700

    Add more data to notification display - repo name and link, update date etc

--- src/Notifications.js
+++ src/Notifications.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { endpoint } from "./api";
+import { notification } from "./Seed";
 
 export default class Notifications extends React.Component {
   constructor(props) {
@@ -7,55 +8,59 @@ export default class Notifications extends React.Component {
     this.state = {
       error: null,
       isLoaded: false,
-      notifications: []
+      notifications: notification
     };
   }
 
-  componentDidMount() {
-    fetch(`${endpoint}/user-notifications.json`, {
-      credentials: "include",
-      mode: "cors"
-    })
-      .then(res => {
-        if (!res.ok) throw new Error('Problem fetching notification data');
-        return res.json();
-      })
-      .then(
-        result => {
-          this.setState({
-            isLoaded: true,
-            notifications: result
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        error => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      );
-  }
+//   componentDidMount() {
+//     fetch(`${endpoint}/user-notifications.json`, {
+//       credentials: "include",
+//       mode: "cors"
+//     })
+//       .then(res => {
+//         if (!res.ok) throw new Error('Problem fetching notification data');
+//         return res.json();
+//       })
+//       .then(
+//         result => {
+//           this.setState({
+//             isLoaded: true,
+//             notifications: result
+//           });
+//         },
+//         // Error handling
+//         error => {
+//           this.setState({
+//             isLoaded: true,
+//             error
+//           });
+//         }
+//       );
+//   }
 
   render() {
     const { error, isLoaded, notifications } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
+    // if (error) {
+    //   return <div>Error: {error.message}</div>;
+    // } else if (!isLoaded) {
+    //   return <div>Loading...</div>;
+    // } else {
       return (
         <div>
           <h2>My Notifications </h2>
-          <ul>
+          <div className="notification-container">
             {notifications.map(n => (
-              <li key={n.id}>{n.subject.title}</li>
+              <div className="notification" key={n.id}>
+              Repo: <a href={n.repository.html_url}>{n.repository.name}</a><br></br>
+                {n.subject.type}: <a href={n.subject.url}>{n.subject.title}</a><br></br>
+                Reason: {n.reason}<br></br>
+                Update date: {n.updated_at.slice(0,10)}
+                <hr></hr>
+              </div>
             ))}
-          </ul>
+          </div>
         </div>
       );
     }
   }
-}
+// }
