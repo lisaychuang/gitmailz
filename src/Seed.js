f8f2c7a27106322b33b43aedcf94ce4432f525c2
Lisa Huang <lisa.yc.huang@gmail.com>
2018-10-03 12:07:58 -0700

    Create a seed data file for development

---
+++ src/Seed.js
@@ -0,0 +1,204 @@
+
+export const notification = [
+  {
+    id: "364112664",
+    unread: true,
+    reason: "comment",
+    updated_at: "2018-09-30T23:34:34.000Z",
+    last_read_at: null,
+    subject: {
+      title: "[feature] Colorize text in node",
+      url: "https://api.github.com/repos/lisaychuang/bite-log/issues/11",
+      latest_comment_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/issues/comments/425760790",
+      type: "Issue"
+    },
+    repository: {
+      id: 143242816,
+      node_id: "MDEwOlJlcG9zaXRvcnkxNDMyNDI4MTY=",
+      name: "bite-log",
+      full_name: "lisaychuang/bite-log",
+      private: false,
+      owner: {
+        login: "lisaychuang",
+        id: 12424987,
+        node_id: "MDQ6VXNlcjEyNDI0OTg3",
+        avatar_url: "https://avatars2.githubusercontent.com/u/12424987?v=4",
+        gravatar_id: "",
+        url: "https://api.github.com/users/lisaychuang",
+        html_url: "https://github.com/lisaychuang",
+        followers_url: "https://api.github.com/users/lisaychuang/followers",
+        following_url:
+          "https://api.github.com/users/lisaychuang/following{/other_user}",
+        gists_url: "https://api.github.com/users/lisaychuang/gists{/gist_id}",
+        starred_url:
+          "https://api.github.com/users/lisaychuang/starred{/owner}{/repo}",
+        subscriptions_url:
+          "https://api.github.com/users/lisaychuang/subscriptions",
+        organizations_url: "https://api.github.com/users/lisaychuang/orgs",
+        repos_url: "https://api.github.com/users/lisaychuang/repos",
+        events_url: "https://api.github.com/users/lisaychuang/events{/privacy}",
+        received_events_url:
+          "https://api.github.com/users/lisaychuang/received_events",
+        type: "User",
+        site_admin: false
+      },
+      html_url: "https://github.com/lisaychuang/bite-log",
+      description:
+        "A bite size colorful and tagged logger for Node and browsers",
+      fork: false,
+      url: "https://api.github.com/repos/lisaychuang/bite-log",
+      forks_url: "https://api.github.com/repos/lisaychuang/bite-log/forks",
+      keys_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/keys{/key_id}",
+      collaborators_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/collaborators{/collaborator}",
+      teams_url: "https://api.github.com/repos/lisaychuang/bite-log/teams",
+      hooks_url: "https://api.github.com/repos/lisaychuang/bite-log/hooks",
+      issue_events_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/issues/events{/number}",
+      events_url: "https://api.github.com/repos/lisaychuang/bite-log/events",
+      assignees_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/assignees{/user}",
+      branches_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/branches{/branch}",
+      tags_url: "https://api.github.com/repos/lisaychuang/bite-log/tags",
+      blobs_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/git/blobs{/sha}",
+      git_tags_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/git/tags{/sha}",
+      git_refs_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/git/refs{/sha}",
+      trees_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/git/trees{/sha}",
+      statuses_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/statuses/{sha}",
+      languages_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/languages",
+      stargazers_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/stargazers",
+      contributors_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/contributors",
+      subscribers_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/subscribers",
+      subscription_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/subscription",
+      commits_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/commits{/sha}",
+      git_commits_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/git/commits{/sha}",
+      comments_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/comments{/number}",
+      issue_comment_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/issues/comments{/number}",
+      contents_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/contents/{+path}",
+      compare_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/compare/{base}...{head}",
+      merges_url: "https://api.github.com/repos/lisaychuang/bite-log/merges",
+      archive_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/{archive_format}{/ref}",
+      downloads_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/downloads",
+      issues_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/issues{/number}",
+      pulls_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/pulls{/number}",
+      milestones_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/milestones{/number}",
+      notifications_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/notifications{?since,all,participating}",
+      labels_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/labels{/name}",
+      releases_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/releases{/id}",
+      deployments_url:
+        "https://api.github.com/repos/lisaychuang/bite-log/deployments"
+    },
+    url: "https://api.github.com/notifications/threads/364112664",
+    subscription_url:
+      "https://api.github.com/notifications/threads/364112664/subscription"
+  },
+  {
+    "id": "1",
+    "repository": {
+      "id": 1296269,
+      "node_id": "MDEwOlJlcG9zaXRvcnkxMjk2MjY5",
+      "name": "Hello-World",
+      "full_name": "octocat/Hello-World",
+      "owner": {
+        "login": "octocat",
+        "id": 1,
+        "node_id": "MDQ6VXNlcjE=",
+        "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+        "gravatar_id": "",
+        "url": "https://api.github.com/users/octocat",
+        "html_url": "https://github.com/octocat",
+        "followers_url": "https://api.github.com/users/octocat/followers",
+        "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+        "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+        "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+        "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+        "organizations_url": "https://api.github.com/users/octocat/orgs",
+        "repos_url": "https://api.github.com/users/octocat/repos",
+        "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+        "received_events_url": "https://api.github.com/users/octocat/received_events",
+        "type": "User",
+        "site_admin": false
+      },
+      "private": false,
+      "html_url": "https://github.com/octocat/Hello-World",
+      "description": "This your first repo!",
+      "fork": false,
+      "url": "https://api.github.com/repos/octocat/Hello-World",
+      "archive_url": "http://api.github.com/repos/octocat/Hello-World/{archive_format}{/ref}",
+      "assignees_url": "http://api.github.com/repos/octocat/Hello-World/assignees{/user}",
+      "blobs_url": "http://api.github.com/repos/octocat/Hello-World/git/blobs{/sha}",
+      "branches_url": "http://api.github.com/repos/octocat/Hello-World/branches{/branch}",
+      "collaborators_url": "http://api.github.com/repos/octocat/Hello-World/collaborators{/collaborator}",
+      "comments_url": "http://api.github.com/repos/octocat/Hello-World/comments{/number}",
+      "commits_url": "http://api.github.com/repos/octocat/Hello-World/commits{/sha}",
+      "compare_url": "http://api.github.com/repos/octocat/Hello-World/compare/{base}...{head}",
+      "contents_url": "http://api.github.com/repos/octocat/Hello-World/contents/{+path}",
+      "contributors_url": "http://api.github.com/repos/octocat/Hello-World/contributors",
+      "deployments_url": "http://api.github.com/repos/octocat/Hello-World/deployments",
+      "downloads_url": "http://api.github.com/repos/octocat/Hello-World/downloads",
+      "events_url": "http://api.github.com/repos/octocat/Hello-World/events",
+      "forks_url": "http://api.github.com/repos/octocat/Hello-World/forks",
+      "git_commits_url": "http://api.github.com/repos/octocat/Hello-World/git/commits{/sha}",
+      "git_refs_url": "http://api.github.com/repos/octocat/Hello-World/git/refs{/sha}",
+      "git_tags_url": "http://api.github.com/repos/octocat/Hello-World/git/tags{/sha}",
+      "git_url": "git:github.com/octocat/Hello-World.git",
+      "issue_comment_url": "http://api.github.com/repos/octocat/Hello-World/issues/comments{/number}",
+      "issue_events_url": "http://api.github.com/repos/octocat/Hello-World/issues/events{/number}",
+      "issues_url": "http://api.github.com/repos/octocat/Hello-World/issues{/number}",
+      "keys_url": "http://api.github.com/repos/octocat/Hello-World/keys{/key_id}",
+      "labels_url": "http://api.github.com/repos/octocat/Hello-World/labels{/name}",
+      "languages_url": "http://api.github.com/repos/octocat/Hello-World/languages",
+      "merges_url": "http://api.github.com/repos/octocat/Hello-World/merges",
+      "milestones_url": "http://api.github.com/repos/octocat/Hello-World/milestones{/number}",
+      "notifications_url": "http://api.github.com/repos/octocat/Hello-World/notifications{?since,all,participating}",
+      "pulls_url": "http://api.github.com/repos/octocat/Hello-World/pulls{/number}",
+      "releases_url": "http://api.github.com/repos/octocat/Hello-World/releases{/id}",
+      "ssh_url": "git@github.com:octocat/Hello-World.git",
+      "stargazers_url": "http://api.github.com/repos/octocat/Hello-World/stargazers",
+      "statuses_url": "http://api.github.com/repos/octocat/Hello-World/statuses/{sha}",
+      "subscribers_url": "http://api.github.com/repos/octocat/Hello-World/subscribers",
+      "subscription_url": "http://api.github.com/repos/octocat/Hello-World/subscription",
+      "tags_url": "http://api.github.com/repos/octocat/Hello-World/tags",
+      "teams_url": "http://api.github.com/repos/octocat/Hello-World/teams",
+      "trees_url": "http://api.github.com/repos/octocat/Hello-World/git/trees{/sha}"
+    },
+    "subject": {
+      "title": "Greetings",
+      "url": "https://api.github.com/repos/octokit/octokit.rb/issues/123",
+      "latest_comment_url": "https://api.github.com/repos/octokit/octokit.rb/issues/comments/123",
+      "type": "Issue"
+    },
+    "reason": "subscribed",
+    "unread": true,
+    "updated_at": "2014-11-07T22:01:45Z",
+    "last_read_at": "2014-11-07T22:01:45Z",
+    "url": "https://api.github.com/notifications/threads/1"
+  }
+];
