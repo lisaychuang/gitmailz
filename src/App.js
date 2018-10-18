3f615044a831a6710a35c8fa28daa48b12cc3145
Lisa Huang <lisa.yc.huang@gmail.com>
2018-10-18 16:34:39 -0700

    Comment out unused code

--- src/App.js
+++ src/App.js
@@ -1,45 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.png';
+// import logo from './logo.png';
 import Router from './Router';
 import PrimarySearchAppBar from './AppBar';
 import Grid from '@material-ui/core/Grid';
-import Paper from "@material-ui/core/Paper";
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      notifications: []
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     notifications: []
+  //   };
+  // }
 
-  handleOnClick = event => {
-    // // Handle Form Submit event default
-    // // Create quote object from state
-    // // Pass quote object to action creator
+  // handleOnClick = event => {
+  //   // // Handle Form Submit event default
+  //   // // Create quote object from state
+  //   // // Pass quote object to action creator
     
-    event.preventDefault();
-    // fetch("https://api.example.com/items")
-    //   .then(res => res.json())
-    //   .then(
-    //     (result) => {
-    //       this.setState({
-    //         isLoaded: true,
-    //         items: result.items
-    //       });
-    //     },
-    //     // Note: it's important to handle errors here
-    //     // instead of a catch() block so that we don't swallow
-    //     // exceptions from actual bugs in components.
-    //     (error) => {
-    //       this.setState({
-    //         isLoaded: true,
-    //         error
-    //       });
-    //     }
-    //   )
-  }
+  //   event.preventDefault();
+  //   // fetch("https://api.example.com/items")
+  //   //   .then(res => res.json())
+  //   //   .then(
+  //   //     (result) => {
+  //   //       this.setState({
+  //   //         isLoaded: true,
+  //   //         items: result.items
+  //   //       });
+  //   //     },
+  //   //     // Note: it's important to handle errors here
+  //   //     // instead of a catch() block so that we don't swallow
+  //   //     // exceptions from actual bugs in components.
+  //   //     (error) => {
+  //   //       this.setState({
+  //   //         isLoaded: true,
+  //   //         error
+  //   //       });
+  //   //     }
+  //   //   )
+  // }
   
   render() {
     return (
