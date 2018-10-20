c227314769cb8eecf766c23bccee332346cee218
Lisa Huang <lisa.yc.huang@gmail.com>
2018-10-20 12:50:11 -0700

    Add comments to endpoint

--- src/api.js
+++ src/api.js
@@ -1,3 +1,7 @@
+// Fetch data from localhost:3000 in local environment
+// OR 
+// Fetch from web API
+
 export const endpoint =
   window.location.host.startsWith("localhost") // If the react app is running on localhost
     ? "http://localhost:3000"   // use localhost:3000 for API calls
