9de6b40680c91d376d1d58c8066d00b90be86e31
Lisa Huang <lisa.yc.huang@gmail.com>
2018-10-23 14:57:34 -0700

    Create redux store with thunk middlewear

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+// combined reducer file for redux
+import rootReducer from "./reducers/index";
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
