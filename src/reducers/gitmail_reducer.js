dd10db4cdf3804ec81668c944ecb8eea9e7e0d0b
Lisa Huang <lisa.yc.huang@gmail.com>
2018-10-23 15:56:18 -0700

    Add switch cases for USER_INFO and FAV_REPOS

--- src/reducers/gitmail_reducer.js
+++ src/reducers/gitmail_reducer.js
@@ -10,12 +10,19 @@ const initialState = {
 
 export default function gimailApp(state = initialState, action) {
     switch (action.type) {
-        case 'LOAD_NOTIFICATIONS': 
+        case 'LOAD_DATA': 
             return { ...state, isLoaded: false};
+
         case 'GET_NOTIFICATIONS_SUCCESS':
             return { ...state, isLoaded: true, notifications: action.payload};
 
-        case 'GET_NOTIFICATIONS_ERROR':
+        case 'GET_USER_INFO':
+            return { ...state, isLoaded: true, userName: action.payload.name, githubUsername: action.payload.username,};
+
+        case 'GET_FAV_REPOS_SUCCESS':
+            return { ...state, isLoaded: true, userFavRepos: action.payload};
+
+        case 'API_ERROR':
             return { ...state, isLoaded: true, error: action.error};
 
         default: 
