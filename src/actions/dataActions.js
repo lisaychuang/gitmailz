9364d4392f6ec74fa8c2e25e416fcb521ba73131
Lisa Huang <lisa.yc.huang@gmail.com>
2018-10-23 15:55:55 -0700

    Define loadUser() and loadFavRepos() action functions

--- src/actions/dataActions.js
+++ src/actions/dataActions.js
@@ -1,13 +1,14 @@
-import { getNotifications } from "../data/requests";
+import { getUserInfo, getNotifications, getUserFavRepos } from "../data/requests";
 
 // const GET_USER_INFO = 'GET_USER_INFO';
 // const GET_FAV_REPOS = 'GET_FAV_REPOS';
 // const SAVE_FAV_REPOS = 'SAVE_FAV_REPOS';
 
+// action function to get NOTIFICATIONS
 export function loadNotifications() {
   return dispatch => {
     dispatch({
-      type: "LOAD_NOTIFICATIONS"
+      type: "LOAD_DATA"
     });
 
     // fetch list of user's notifications from Rails API
@@ -21,9 +22,66 @@ export function loadNotifications() {
     // Catch error if the API response never came, e.g. if user is offline or request timed out
     .catch(
       error => {
-        window.alert("Current user request never came back");
+        console.error("Current user web request never came back");
         dispatch({
-          type: "GET_NOTIFICATIONS_ERROR",
+          type: "API_ERROR",
+          error
+        });
+      }
+    );
+  };
+}
+
+
+// action function to get USER INFO
+export function loadUser() {
+  return dispatch => {
+    dispatch({
+      type: "LOAD_DATA"
+    });
+
+    // fetch list of user's notifications from Rails API
+    getUserInfo()
+    .then(data => {
+      dispatch({
+        type: "GET_USER_INFO",
+        payload: data
+      });
+    })
+    // Catch error if the API response never came, e.g. if user is offline or request timed out
+    .catch(
+      error => {
+        console.error("Current user request never came back");
+        dispatch({
+          type: "API_ERROR",
+          error
+        });
+      }
+    );
+  };
+}
+
+// action function to get FAV REPOS
+export function loadFavRepos() {
+  return dispatch => {
+    dispatch({
+      type: "LOAD_DATA"
+    });
+
+    // fetch list of user's notifications from Rails API
+    getUserFavRepos()
+    .then(data => {
+      dispatch({
+        type: "GET_FAV_REPOS_SUCCESS",
+        payload: data
+      });
+    })
+    // Catch error if the API response never came, e.g. if user is offline or request timed out
+    .catch(
+      error => {
+        console.error("Fav repo web request never came back");
+        dispatch({
+          type: "API_ERROR",
           error
         });
       }
